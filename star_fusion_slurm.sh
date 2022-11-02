6e507bf9690f0698717a82739b1ec3b853638e67
Charlee <64256047+CHARALEX279@users.noreply.github.com>
2022-11-01 20:01:11 -0400

    Updated Fusion antigen workflow
    Agfusion/pVACfuse are used to locate neoantigens caused by fusions. AGFusion depends on Star-Fusion .tsv outputs

---
+++ star_fusion_slurm.sh
@@ -0,0 +1,14 @@
+#running star-fusion to obtain tsv file.
+#note, this is run in the shared folder, /scratch/dq2033-share
+#!/bin/bash
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=10:00:00
+#SBATCH --mem=16GB
+#SBATCH --job-name=test
+module purge
+module load 
+STAR-Fusion  --left_fq reads_1.fq \
+             --right_fq reads_2.fq \
+             --output_dir star_fusion_outdir/star-fusion.fusion_predictions.tsv
