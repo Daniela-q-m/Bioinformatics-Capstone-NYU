c1bb9bf44037e6a1b282a35182893eaa071bb9db
Charlee <64256047+CHARALEX279@users.noreply.github.com>
2022-11-11 16:48:32 -0500

    First draft in making star fusion files
    imported from: https://github.com/STAR-Fusion/STAR-Fusion/blob/master/Docker/__test_run_Singularity.sh
    need to update paths to local folders

---
+++ make_star_fusion_files.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=10:00:00
+#SBATCH --mem=4GB
+#SBATCH --job-name=test
+module purge
+
+set -ve
+
+if [ -z ${CTAT_GENOME_LIB} ]; then
+    echo "Error, must have CTAT_GENOME_LIB env var set"
+    exit 1
+fi
+
+VERSION=`cat VERSION.txt`
+
+# run STAR-Fusion
+cd ../ && singularity exec -e -B ${CTAT_GENOME_LIB}:/ctat_genome_lib Docker/star-fusion.v${VERSION}.simg\
+/usr/local/src/STAR-Fusion/STAR-Fusion\
+--left_fq testing/reads_1.fq.gz\
+--right_fq testing/reads_2.fq.gz\
+--genome_lib_dir /ctat_genome_lib\
+-O testing/test_singularity_outdir/StarFusionOut\
+--FusionInspector inspect --examine_coding_effect --denovo_reconstruct
+
